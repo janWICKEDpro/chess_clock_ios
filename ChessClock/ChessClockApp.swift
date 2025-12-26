2a154f7bccc65eb1d1b9c5bd367230cae113a44a
janWICKEDpro <janroyal45@gmail.com>
2025-12-26 07:53:32 +0100

    Initial Commit

---
+++ ChessClock/ChessClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessClockApp.swift
+//  ChessClock
+//
+//  Created by Jan Royal on 26/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChessClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChessClockTests/ChessClockTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChessClockTests.swift
+//  ChessClockTests
+//
+//  Created by Jan Royal on 26/12/2025.
+//
+
+import Testing
+@testable import ChessClock
+
+struct ChessClockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
